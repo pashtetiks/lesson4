989ca0069f60ed1f07b152e35e30a97c23ef200b
79875 <pipochinka@edu.hse.ru>
2022-07-19 14:38:36 +0300

    lesson5 init

--- src/main/java/ru/pashtet/lesson5/MainApp.java
+++ src/main/java/ru/pashtet/lesson5/MainApp.java
@@ -1,7 +1,57 @@
 package ru.pashtet.lesson5;
 
 public class MainApp {
+    static final int size = 10000000;
+    static final int h = size / 2;
     public static void main(String[] args) {
+        firstMethod();
+        secondMethod();
+
+
+    }
+    public static void firstMethod(){
+        float[] arr = new float[size];
+        for(float i: arr){
+            i = 1;
+        }
+        long a = System.currentTimeMillis();
+        for(int i = 0; i < arr.length; i++){
+            arr[i] = (float)(arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+        }
+        System.out.println(System.currentTimeMillis()-a);
+    }
+
+    public static void secondMethod(){
+        float[] arr = new float[size];
+        for(float i: arr){
+            i = 1;
+        }
+        long a = System.currentTimeMillis();
+        float[] a1 = new float[h];
+        float[] a2 = new float[h];
+        System.arraycopy(arr, 0, a1, 0, h);
+        System.arraycopy(arr, h, a2, 0, h);
+        Thread thread1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < a1.length; i++){
+                    a1[i] = (float)(a1[(int) i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+                }
+            }
+        });
+        Thread thread2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < a2.length; i++){
+                    a2[i] = (float)(a2[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+                }
+            }
+        });
+        thread1.start();
+        thread2.start();
+        System.arraycopy(a1, 0, arr, 0, h);
+        System.arraycopy(a2, 0, arr, h, h);
+        System.out.println(System.currentTimeMillis() - a);
 
     }
 }
